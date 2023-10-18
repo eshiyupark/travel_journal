e3bc11b3e313e176577e513a573178e10ff34914
eshiyu park <eshiyupark@gmail.com>
2023-10-18 14:16:30 +1100

    Created mock data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import data from './data.js'
 
 function App() {
 
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: "Tokyo",
+    location: "Japan",
+    googleMapsUrl: "https://maps.app.goo.gl/hokYmZsTYcWbkhdX7",
+    startDate: "17 September 2023",
+    endDate: "28 September 2023",
+    description: "Tokyo, Japan's busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum).",
+    imageUrl: "https://lp-cms-production.imgix.net/2020-11/shutterstockRF_257639824.jpg?auto=format&w=1440&h=810&fit=crop&q=75"
+  },
+  {
+    title: "Taipei City",
+    location: "Taiwan",
+    googleMapsUrl: "https://maps.app.goo.gl/kpZxS3ag5nQBstA68",
+    startDate: "29 September 2023",
+    endDate: "06 October 2023",
+    description: "Taipei, the capital of Taiwan, is a modern metropolis with Japanese colonial lanes, busy shopping streets and contemporary buildings. The skyline is crowned by the 509m-tall, bamboo-shaped Taipei 101 skyscraper, with upscale shops at the base and a rapid elevator to an observatory near the top. Taipei is also known for its lively street-food scene and many night markets, including expansive Shilin market.",
+    imageUrl: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/10/16/09/taipei-overview.jpg?quality=75&width=990&crop=3%3A2%2Csmart&auto=webp"
+  },
+  {
+    title: "Hong Kong",
+    location: "Hong Kong",
+    googleMapsUrl: "https://maps.app.goo.gl/X54vs9jb232iDF2y9",
+    startDate: "07 October 2023",
+    endDate: "13 October 2023",
+    description: "Hong Kong, officially the Hong Kong Special Administrative Region of the People's Republic of China, is a city and a special administrative region in China.",
+    imageUrl: "https://www.discoverhongkong.com/content/dam/dhk/intl/explore/attractions/best-ways-to-marvel-at-iconic-victoria-harbour/best-ways-to-marvel-at-iconic-victoria-harbour-1920x1080.jpg"
+  },
+  {
+    title: "Koh Tao",
+    location: "Thailand",
+    googleMapsUrl: "https://maps.app.goo.gl/LSdzugreGPUCmjcn6",
+    startDate: "10 September 2023",
+    endDate: "16 September 2023",
+    description: "Island offering scenic white sand beaches, coral reefs & scuba diving, with numerous resorts & bars.",
+    imageUrl: "https://a.cdn-hotels.com/gdcs/production148/d51/200ef146-c064-4e16-b05d-ab17a443cf3e.jpg?impolicy=fcrop&w=800&h=533&q=medium"
+  },
+  {
+    title: "Bangkok",
+    location: "Thailand",
+    googleMapsUrl: "https://maps.app.goo.gl/hAESMAevQvLHohw78",
+    startDate: "02 September 2023",
+    endDate: "09 September 2023",
+    description: "Bangkok, Thailand's capital, is a large city known for ornate shrines and vibrant street life. The boat-filled Chao Phraya River feeds its network of canals, flowing past the Rattanakosin royal district, home to opulent Grand Palace and its sacred Wat Phra Kaew Temple. Nearby is Wat Pho Temple with an enormous reclining Buddha and, on the opposite shore, Wat Arun Temple with its steep steps and Khmer-style spire.",
+    imageUrl: "https://a.cdn-hotels.com/gdcs/production172/d459/3af9262b-3d8b-40c6-b61d-e37ae1aa90aa.jpg"
+  }
+]
